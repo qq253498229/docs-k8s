6e29095b27f7abc77f7cc085634915135deda486
wangbin <253498229@qq.com>
2019-03-03 04:39:57 +0800

    polish

--- ingress/install.sh
+++ ingress/install.sh
@@ -4,4 +4,5 @@ docker pull registry.cn-qingdao.aliyuncs.com/wangdali/nginx-ingress-controller:0
 docker tag registry.cn-qingdao.aliyuncs.com/wangdali/nginx-ingress-controller:0.22.0 quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.22.0
 docker rmi registry.cn-qingdao.aliyuncs.com/wangdali/nginx-ingress-controller:0.22.0
 
-kubectl apply -f mandatory.yaml
+#kubectl apply -f mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/qq253498229/docs-k8s/master/ingress/mandatory.yaml

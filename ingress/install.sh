83795dedf8dd12704c3277d34fcb72d2bcc004c3
王斌 <253498229@qq.com>
2019-03-16 01:55:34 +0800

    polish

--- ingress/install.sh
+++ ingress/install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-docker pull registry.cn-qingdao.aliyuncs.com/wangdali/nginx-ingress-controller:0.22.0
-docker tag registry.cn-qingdao.aliyuncs.com/wangdali/nginx-ingress-controller:0.22.0 quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.22.0
-docker rmi registry.cn-qingdao.aliyuncs.com/wangdali/nginx-ingress-controller:0.22.0
+docker pull registry.cn-qingdao.aliyuncs.com/wangdali/nginx-ingress-controller:0.23.0
+docker tag registry.cn-qingdao.aliyuncs.com/wangdali/nginx-ingress-controller:0.23.0 quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.23.0
+docker rmi registry.cn-qingdao.aliyuncs.com/wangdali/nginx-ingress-controller:0.23.0
 
 #kubectl apply -f mandatory.yaml
 kubectl apply -f https://raw.githubusercontent.com/qq253498229/docs-k8s/master/ingress/mandatory.yaml
--- shell/pull-google-images.sh
+++ shell/pull-google-images.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-images=(kube-proxy:v1.13.3 kube-apiserver:v1.13.3 kube-controller-manager:v1.13.3 kube-scheduler:v1.13.3 coredns:1.2.6 etcd:3.2.24 pause:3.1)
+images=(
+kube-proxy:v1.13.4
+kube-apiserver:v1.13.4
+kube-controller-manager:v1.13.4
+kube-scheduler:v1.13.4
+coredns:1.2.6
+etcd:3.2.24
+pause:3.1
+)
 
 for image in ${images[@]}; do
-  docker pull registry.cn-beijing.aliyuncs.com/wangdali/$image
-  docker tag registry.cn-beijing.aliyuncs.com/wangdali/$image k8s.gcr.io/$image
-  docker rmi registry.cn-beijing.aliyuncs.com/wangdali/$image
+  docker pull registry.cn-beijing.aliyuncs.com/k8s-docker-image/$image
+  docker tag registry.cn-beijing.aliyuncs.com/k8s-docker-image/$image k8s.gcr.io/$image
+  docker rmi registry.cn-beijing.aliyuncs.com/k8s-docker-image/$image
 done
